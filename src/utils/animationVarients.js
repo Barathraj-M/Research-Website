49f9001a6bc010a7daddf332d7f69db2e0a5c40a
AnandhaSivam-00 <anandsugan218@gmail.com>
2025-08-08 10:01:13 +0530

    chore: Added a new animation variants

--- src/utils/animationVarients.js
+++ src/utils/animationVarients.js
@@ -297,3 +297,23 @@ export const quoteIconDanceVariants = {
         }
     }
 };
+
+export const shufflingTextBoxes = {
+    initial: {
+        opacity: 0, 
+        scale: 0.8
+    },
+    animate: {
+        opacity: 1, 
+        scale: 1
+    },
+    exit: {
+        opacity: 0, 
+        scale: 0.8
+    },
+    transition: {
+        type: 'spring', 
+        stiffness: 300, 
+        damping: 30
+    }
+}
